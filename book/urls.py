be8ef41d633caaec829d9543de146e5d38a1174e
Nguyen Quang Thang <102580944+thang44hdai@users.noreply.github.com>
2025-02-25 21:31:00 +0700

    rest api book

--- book/urls.py
+++ book/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),
+    path('api/books/', views.BookListAPI.as_view(), name='book-list'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response  # Import đúng từ DRF
 
 from book.models import Book
+from book.serializers import BookSerializer
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer  # Thêm JSONRenderer
 
 # Create your views here.
 
@@ -8,3 +12,12 @@ from book.models import Book
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'book_list.html', {'books': books})
+
+
+class BookListAPI(APIView):
+    renderer_classes = [JSONRenderer]  # Luôn trả về JSON
+
+    def get(self, request, *args, **kwargs):
+        books = Book.objects.using("mongodb").all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
